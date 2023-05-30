3e1e461b2853e23c3f817d2d8dffbf8258d6c9c8
kumar4372 <gautamkumar4372@gmail.com>
2023-05-30 18:37:47 +0900

    add train files

--- data/uee_dataset/merge.sh
+++ data/uee_dataset/merge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat train_uee_221212_txt_part1.json train_uee_221212_txt_part2.json > train_uee_221212_txt.json
+cat train_uee_221212_txt_*.json > train_uee_221212_txt.json
 
 python3 merge-ann-and-txt-uee.py \
 	-o train_uee_221212.json \
